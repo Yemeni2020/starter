e6b0bc2d86cd16b73c1d036de324d84f9777a587
Osama <Osm540@hotmail.com>
2026-01-14 14:30:53 +0300

    fix: register seo and security settings providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
+    App\Providers\SeoServiceProvider::class,
+    App\Providers\SecuritySettingsServiceProvider::class,
 ];
